a362b8fabb18a38eec4f35cd891899f7345ac015
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-01-29 00:28:32 +0100

    removed comments

--- LetMeCook/model/Recipe.cs
+++ LetMeCook/model/Recipe.cs
@@ -5,21 +5,15 @@ namespace LetMeCook.model;
 
 public class Recipe
 {
-    [BsonId] // This attribute indicates that this property is the primary key
-    public ObjectId Id { get; set; }
+    [BsonId] public ObjectId Id { get; set; }
 
-    [BsonElement("title")] // Maps to the "title" field in BSON
-    public string Title { get; set; }
+    [BsonElement("title")] public string Title { get; set; }
 
-    [BsonElement("ingredients")] // Maps to the "ingredients" field in BSON
-    public List<string> Ingredients { get; set; }
+    [BsonElement("ingredients")] public List<string> Ingredients { get; set; }
 
-    [BsonElement("instructions")] // Maps to the "instructions" field in BSON
-    public List<string> Instructions { get; set; }
+    [BsonElement("instructions")] public List<string> Instructions { get; set; }
 
-    [BsonElement("score")] // Maps to the "score" field in BSON
-    public double Score { get; set; }
+    [BsonElement("score")] public double Score { get; set; }
 
-    [BsonElement("createdBy")] // Maps to the "createdBy" field in BSON
-    public string CreatedBy { get; set; }
+    [BsonElement("createdBy")] public string CreatedBy { get; set; }
 }
